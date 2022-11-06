5ca314a1344b37c912fda9b3363fa61ad48a49d5
weecoding <weecoding@gmail.com>
2022-11-07 04:16:17 +0900

    #2.3 const and let
    #2.3 const and let

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello'); 
+const a = 10; 
+const b = 2;
+let myName = 'nico';  
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log('hello '+myName);
+
+myName = 'nicolas'
+
+console.log('your new name is '+myName)
